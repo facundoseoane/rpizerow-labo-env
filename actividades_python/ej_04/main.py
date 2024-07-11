489a3950c888bede62ef74a4eb43191a856f96d7
facundoseoane <128711918+facundoseoane@users.noreply.github.com>
2024-07-11 13:55:36 -0300

    Update main.py

--- actividades_python/ej_04/main.py
+++ actividades_python/ej_04/main.py
@@ -4,19 +4,13 @@ import ADS1x15
 import math
 from gpiozero import PWMLED
 
-# choose your sensor
-# ADS = ADS1x15.ADS1013(1, 0x48)
-# ADS = ADS1x15.ADS1014(1, 0x48)
-# ADS = ADS1x15.ADS1015(1, 0x48)
-# ADS = ADS1x15.ADS1113(1, 0x48)
-# ADS = ADS1x15.ADS1114(1, 0x48)
 
 ADS = ADS1x15.ADS1115(1, 0x48)
 
 print(os.path.basename(__file__))
 print("ADS1X15_LIB_VERSION: {}".format(ADS1x15.__version__))
 
-# set gain to 4.096V max
+
 ADS.setGain(ADS.PGA_4_096V)
 f = ADS.toVoltage()
 
@@ -26,18 +20,18 @@ azul = PWMLED(26)
 rojo = PWMLED(19)
 
 while True :
-    val_ter = ADS.readADC(1)
-    val_pot = ADS.readADC(3)
-    volt_term = val_ter * f
+    val_termistor = ADS.readADC(1)
+    val_pote = ADS.readADC(3)
+    volt_termistor = val_ter * f
 
-    temp_pot = 30 * val_pot / 32767.5
-    res_ter = (volt_term * 10000) / (3.3 - volt_term)
-    temp_term = (298 * 3900) / (298 * math.log(res_ter / 10000) + 3900) - 273.15
+    temp_pote = 30 * val_pote / 32767.5
+    res_termistor = (volt_termistor * 10000) / (3.3 - volt_termistor)
+    temp_termistor = (298 * 3900) / (298 * math.log(res_termistor / 10000) + 3900) - 273.15
 
-    print(temp_term)
-    print(temp_pot)
+    print(temp_termistor)
+    print(temp_pote)
 
-    dif = temp_term - temp_pot
+    dif = temp_termistor - temp_pote
 
     if dif > 0:
         if dif >= 5:
